82ca51875ae288774f5ff05774075eabfda9abea
KaikeWSL <kaike.wesley12@gmail.com>
2025-10-07 08:10:43 -0300

    Delete src directory

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Sistema de Cálculo Excel Profissional
-Pacote principal do sistema
-"""
-
-__version__ = "1.0.0"
-__author__ = "Mega Systems"
-__description__ = "Sistema profissional para cálculos em planilhas Excel"
